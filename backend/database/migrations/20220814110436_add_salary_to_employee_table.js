551029a3472ca2360ff205d4c18a9a8fa73babb9
ljenchik <84686704+ljenchik@users.noreply.github.com>
2022-08-14 12:14:58 +0100

    Added migration: salary column in employee table

---
+++ backend/database/migrations/20220814110436_add_salary_to_employee_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table("employee", function (table) {
+    table.double("salary").nullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table("employee", function (table) {
+    table.dropColumn("salary");
+  });
+};
